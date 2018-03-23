743c9554d39ed4798727d014d3bdbfa6ab563388
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-03-23 14:07:09 -0300

    Add movie models

---
+++ CsBootcamp/Models/Movie.swift
@@ -0,0 +1,19 @@
+//
+//  Movie.swift
+//  CsBootcamp
+//
+//  Created by Andre Rodrigues on 23/03/18.
+//  Copyright © 2018 Bootcampers. All rights reserved.
+//
+
+import Foundation
+
+struct Movie {
+    
+    let id: Int
+    let genreIds: [Int]
+    let title: String
+    let overview: String
+    let releaseDate: Date
+    let posterUrl: URL
+}
