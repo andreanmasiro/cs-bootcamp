925c1ee5aa91a55f5214eafef0b7f9e917dc69b2
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-03-23 18:39:53 -0300

    Show initial view controller in main window

--- CsBootcamp/AppDelegate.swift
+++ CsBootcamp/AppDelegate.swift
@@ -15,6 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let rootViewController = MoviesListViewController()
+        let presenter = MoviesListPresenter(view: rootViewController)
+        let interactor = MoviesListInteractor(presenter: presenter)
+        rootViewController.interactor = interactor
+        
+        window?.rootViewController = rootViewController
+        window?.makeKeyAndVisible()
+        
         return true
     }
 }
