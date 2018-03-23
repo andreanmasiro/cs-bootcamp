dde8472cc03dd870a94d6ac68fbae6bc07424b9a
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-03-23 16:57:46 -0300

    Add app color constants

---
+++ CsBootcamp/Colors/UIColor+App.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+App.swift
+//  CsBootcamp
+//
+//  Created by Andre Rodrigues on 23/03/18.
+//  Copyright © 2018 Bootcampers. All rights reserved.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+    
+    enum Bootcamp { 
+        
+        static let darkBlue = #colorLiteral(red: 0.1764705882, green: 0.1882352941, blue: 0.2745098039, alpha: 1)
+        static let yellow = #colorLiteral(red: 0.9843137255, green: 0.8196078431, blue: 0.4, alpha: 1)
+    }
+}
