3d01331e9ba1f22d1da03ce14dedfdd779a5d619
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-03-23 16:20:27 -0300

    Add reusable view protocol

---
+++ CsBootcamp/ReusableView/ReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ReusableView.swift
+//  CsBootcamp
+//
+//  Created by Andre Rodrigues on 23/03/18.
+//  Copyright © 2018 Bootcampers. All rights reserved.
+//
+
+protocol ReusableView {
+    
+    static var reuseIdentifier: String { get }
+}
