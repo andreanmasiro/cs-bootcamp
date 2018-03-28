389f785eb39bbc0b65d80586bbbfaa96fe0f398b
cs-lucas-nascimento <lucas.nascimento@concrete.com>
2018-03-28 17:01:13 -0300

    Refactor MovieTextTableViewCellSpec

--- CsBootcampTests/MovieDetail/Views/MovieTextTableViewCellSpec.swift
+++ CsBootcampTests/MovieDetail/Views/MovieTextTableViewCellSpec.swift
@@ -16,53 +16,20 @@ class MovieTextTableViewCellSpec: QuickSpec {
     override func spec() {
         
         typealias ViewModel = MovieDetailViewController.ViewModel
-        typealias PosterViewModel = MoviePosterTableViewCell.ViewModel
-        typealias TextViewModel = MovieTextTableViewCell.ViewModel
-        typealias OverviewViewModel = MovieOverviewTableViewCell.ViewModel
         
         describe("MovieTextTableViewCell") {
         
-            var movie: Movie!
-            var genre1: Genre!
-            var genre2: Genre!
-            var genreId1: Int!
-            var genreId2: Int!
-            var genres: [Int]!
-
-            var viewModel: ViewModel!
-            var posterViewModel: PosterViewModel!
-            var releaseDateViewModel: TextViewModel!
-            var genresViewModel: TextViewModel!
-            var overviewViewModel: OverviewViewModel!
-            
             var sutReleaseDate : MovieTextTableViewCell!
             var sutRGenres : MovieTextTableViewCell!
             var tableView: UITableView!
+            var viewModel: ViewModel!
             var dataSource: MovieDetailDataSource!
-            var cell: UITableViewCell!
             var indexPathRow_1: IndexPath!
             var indexPathRow_2: IndexPath!
             
             beforeSuite {
-                genre1 = Genre(id: 1, name: "Sci-Fi")
-                genre2 = Genre(id: 2, name: "Action")
-                genreId1 = genre1.id
-                genreId2 = genre2.id
-                genres = [genreId1, genreId2]
-                movie = Movie(id: 1,
-                              genreIds: genres,
-                              title: "Star Wars - The Last Jedi",
-                              overview: "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares for battle with the First Order.",
-                              releaseDate: Date(),
-                              posterUrl: URL(string: "https://www.elastic.co/assets/bltada7771f270d08f6/enhanced-buzz-1492-1379411828-15.jpg")!)
-  
-                posterViewModel = PosterViewModel(imageURL: movie.posterUrl, title: movie.title)
-                releaseDateViewModel = TextViewModel(description: movie.releaseDate.yearToString())
-                genresViewModel = TextViewModel(description: "\(genre1), \(genre2)")
-                overviewViewModel = OverviewViewModel(overview: movie.overview)
-                
-                viewModel = ViewModel(poster: posterViewModel, releaseDate: releaseDateViewModel, genres: genresViewModel, overview: overviewViewModel)
-                
+        
+                viewModel = ViewModelBuilder.build()
                 tableView = UITableView(frame: .zero)
                 indexPathRow_1 = IndexPath(row: 1, section: 0)
                 indexPathRow_2 = IndexPath(row: 2, section: 0)
