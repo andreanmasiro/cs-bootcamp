b60ea4be9be16a8bbdfe6e19ff111f432aa909d2
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-03-28 18:03:45 -0300

    Refactor movies list view controller tests structure

--- CsBootcampTests/Scenes/MoviesList/ViewController/MoviesListViewControllerSpec.swift
+++ CsBootcampTests/Scenes/MoviesList/ViewController/MoviesListViewControllerSpec.swift
@@ -21,18 +21,6 @@ class MoviesListViewControllerSpec: QuickSpec {
             
             context("when initialized") {
                 
-                context("with coder") {
-                    
-                    beforeEach {
-                        let coder = NSCoder()
-                        viewController = MoviesListViewController(coder: coder)
-                    }
-                    
-                    it("should be nil") {
-                        expect(viewController).to(beNil())
-                    }
-                }
-                
                 beforeEach {
                     viewController = MoviesListViewController()
                 }
@@ -51,78 +39,87 @@ class MoviesListViewControllerSpec: QuickSpec {
                     expect(viewController.view.subviews)
                         .to(contain(expectedSubviews))
                 }
+                
+                context("and view will appear") {
+                    
+                    let interactor = MoviesListInteractorSpy()
+                    
+                    beforeEach {
+                        viewController.interactor = interactor
+                        viewController.beginAppearanceTransition(true, animated: false)
+                        viewController.endAppearanceTransition()
+                    }
+                    
+                    it("should change to loading state") {
+                        
+                        expect(viewController.activityIndicator.isAnimating)
+                            .to(beTrue())
+                    }
+                    
+                    it("should fetch movies") {
+                        
+                        expect(interactor.fetchMoviesCalled).to(beTrue())
+                    }
+                }
+                
+                context("and display movies is called") {
+                    
+                    let viewModel = MoviesListViewModel(cellViewModels: (0..<3).map { _ in
+                        MovieCollectionViewCell.ViewModel(
+                            imageURL: URL(string: "url.com")!,
+                            title: ""
+                        )
+                    })
+                    beforeEach {
+                        
+                        viewController.displayMovies(viewModel: viewModel)
+                    }
+                    
+                    it("should change to list state") {
+                        
+                        expect(viewController.collectionView.isHidden).to(beFalse())
+                    }
+                    
+                    it("should set viewmodels to data source") {
+                        
+                        expect(viewController.dataSource.viewModels)
+                            .to(equal(viewModel.cellViewModels))
+                    }
+                }
+                
+                context("and display error is called") {
+                    
+                    let errorViewModel = MoviesListErrorViewModel()
+                    
+                    beforeEach {
+                        
+                        viewController.displayError(viewModel: errorViewModel)
+                    }
+                    
+                    it("should change to error state") {
+                        
+                        expect(viewController.errorView.isHidden).to(beFalse())
+                    }
+                    
+                    it("should setup error view") {
+                        
+                        expect(viewController.errorView.imageView.image)
+                            .to(equal(errorViewModel.image))
+                        expect(viewController.errorView.label.text)
+                            .to(match(errorViewModel.message))
+                    }
+                }
             }
             
-            context("when view will appear") {
-                
-                let interactor = MoviesListInteractorSpy()
+            context("when initialized with coder") {
                 
                 beforeEach {
-                    viewController = MoviesListViewController()
-                    viewController.interactor = interactor
-                    viewController.beginAppearanceTransition(true, animated: false)
-                    viewController.endAppearanceTransition()
+                    let coder = NSCoder()
+                    viewController = MoviesListViewController(coder: coder)
                 }
                 
-                it("should change to loading state") {
-                    
-                    expect(viewController.activityIndicator.isAnimating)
-                        .to(beTrue())
-                }
-                
-                it("should fetch movies") {
-                    
-                    expect(interactor.fetchMoviesCalled).to(beTrue())
-                }
-            }
-            
-            context("when display movies is called") {
-                
-                let viewModel = MoviesListViewModel(cellViewModels: (0..<3).map { _ in
-                    MovieCollectionViewCell.ViewModel(
-                        imageURL: URL(string: "url.com")!,
-                        title: ""
-                    )
-                })
-                beforeEach {
-                    
-                    viewController = MoviesListViewController()
-                    viewController.displayMovies(viewModel: viewModel)
-                }
-                
-                it("should change to list state") {
-                    
-                    expect(viewController.collectionView.isHidden).to(beFalse())
-                }
-                
-                it("should set viewmodels to data source") {
-                    
-                    expect(viewController.dataSource.viewModels)
-                        .to(equal(viewModel.cellViewModels))
-                }
-            }
-            
-            context("when display error is called") {
-                
-                let errorViewModel = MoviesListErrorViewModel()
-                
-                beforeEach {
-                    
-                    viewController = MoviesListViewController()
-                    viewController.displayError(viewModel: errorViewModel)
-                }
-                
-                it("should change to error state") {
-                    
-                    expect(viewController.errorView.isHidden).to(beFalse())
-                }
-                
-                it("should setup error view") {
-                    
-                    expect(viewController.errorView.imageView.image)
-                        .to(equal(errorViewModel.image))
-                    expect(viewController.errorView.label.text)
-                        .to(match(errorViewModel.message))
+                it("should be nil") {
+                    expect(viewController).to(beNil())
                 }
             }
             
