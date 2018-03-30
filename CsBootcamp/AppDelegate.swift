ed26a7677c7d207a009624a1b4acfe54e0834987
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-03-29 21:38:20 -0300

    Remove test verification from app delegate, add to main

--- CsBootcamp/AppDelegate.swift
+++ CsBootcamp/AppDelegate.swift
@@ -7,32 +7,26 @@
 //
 
 import UIKit
-import Moya
 
-@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
-        let isTesting = NSClassFromString("XCTestCase") != nil
-        if !isTesting {
-            
-            let rootViewController = MoviesListSceneFactory.make()
-            
-            let navigationController = UINavigationController(rootViewController: rootViewController)
-            navigationController.navigationBar.barTintColor = UIColor.Bootcamp.yellow
-            navigationController.navigationBar.isTranslucent = false
-            let screen = UIScreen.main
-            
-            let window = UIWindow(frame: screen.bounds)
-            window.rootViewController = navigationController
-            window.backgroundColor = UIColor.white
-            window.makeKeyAndVisible()
-            
-            self.window = window
-        }
+        
+        let rootViewController = MoviesListSceneFactory.make()
+        
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        navigationController.navigationBar.barTintColor = UIColor.Bootcamp.yellow
+        navigationController.navigationBar.isTranslucent = false
+        let screen = UIScreen.main
+        
+        let window = UIWindow(frame: screen.bounds)
+        window.rootViewController = navigationController
+        window.backgroundColor = UIColor.white
+        window.makeKeyAndVisible()
+        
+        self.window = window
         return true
     }
 }
--- CsBootcamp/main.swift
+++ CsBootcamp/main.swift
@@ -7,3 +7,19 @@
 //
 
 import Foundation
+import UIKit
+
+class UnitTestAppDelegate: NSObject, UIApplicationDelegate { }
+
+let isTesting = NSClassFromString("XCTestCase") != nil
+
+let argc = CommandLine.argc
+let argv = UnsafeMutableRawPointer(CommandLine.unsafeArgv).bindMemory(to: UnsafeMutablePointer<Int8>.self, capacity: Int(CommandLine.argc))
+
+#if TEST
+
+UIApplicationMain(argc, argv, nil, NSStringFromClass(UnitTestAppDelegate.self))
+#else
+    
+UIApplicationMain(argc, argv, nil, NSStringFromClass(AppDelegate.self))
+#endif
