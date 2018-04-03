916d7f811530dde73c3fbe5a85800abb99810793
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-04-03 14:05:09 -0300

    Perform pod install to fix binary incompatibility

---
+++ CsBootcamp/Scenes/MoviesList/ViewController/SearchBarDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SearchBarDelegate.swift
+//  CsBootcamp
+//
+//  Created by Andre Rodrigues on 03/04/18.
+//  Copyright © 2018 Bootcampers. All rights reserved.
+//
+
+import UIKit
+
+class SearchBarDelegate: NSObject {
+
+}
