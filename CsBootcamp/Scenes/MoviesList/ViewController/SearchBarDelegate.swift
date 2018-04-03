f42ba93fb840df4582cf6e3bea828a19a6552a86
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-04-03 14:05:47 -0300

    Add search bar delegate

--- CsBootcamp/Scenes/MoviesList/ViewController/SearchBarDelegate.swift
+++ CsBootcamp/Scenes/MoviesList/ViewController/SearchBarDelegate.swift
@@ -8,6 +8,38 @@
 
 import UIKit
 
-class SearchBarDelegate: NSObject {
-
+class SearchBarDelegate: NSObject, UISearchBarDelegate {
+    
+    var textDidChange: ((String) -> ())?
+    
+    init(searchBar: UISearchBar) {
+        super.init()
+        searchBar.delegate = self
+    }
+    
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+    
+    func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
+        searchBar.setShowsCancelButton(false, animated: true)
+    }
+    
+    public func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
+        searchBar.text.map { text in
+            self.textDidChange?(text)
+        }
+    }
+    
+    public func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        textDidChange?("")
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        textDidChange?(searchText)
+    }
 }
