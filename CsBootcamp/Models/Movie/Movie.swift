2f022cd93f60befbd171d9e5aa8bf98aa1f14d3a
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-04-04 17:02:57 -0300

    Replace codable with decodable from models

--- CsBootcamp/Models/Movie/Movie.swift
+++ CsBootcamp/Models/Movie/Movie.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Movie: Codable {
+struct Movie: Decodable {
     
     let id: Int
     let genreIds: [Int]
@@ -16,24 +16,4 @@ struct Movie: Codable {
     let overview: String
     let releaseDate: Date
     let posterPath: String
-    
-    init(id: Int, genreIds: [Int], title: String, overview: String, releaseDate: Date, posterPath: String) {
-        
-        self.id = id
-        self.genreIds = genreIds
-        self.title = title
-        self.overview = overview
-        self.releaseDate = releaseDate
-        self.posterPath = posterPath
-    }
-    
-    enum CodingKeys: String, CodingKey {
-        
-        case id
-        case genreIds = "genre_ids"
-        case title
-        case overview
-        case releaseDate = "release_date"
-        case posterPath = "poster_path"
-    }
 }
