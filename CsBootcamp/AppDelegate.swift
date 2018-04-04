6eca59d168c0afc0bb3e8647b059f92f11331464
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-04-04 17:02:29 -0300

    Merge from local stash

--- CsBootcamp/AppDelegate.swift
+++ CsBootcamp/AppDelegate.swift
@@ -7,30 +7,33 @@
 //
 
 import UIKit
+import CoreData
+import Foundation
 
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let coreDataStack = CoreDataStack()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
+
         let moviesListViewController = MoviesListSceneFactory.make()
         let favoritesListViewController = FavoritesListSceneFactory.make()
-        
+
         let moviesNavigationController = UINavigationControllerFactory.make(with: moviesListViewController)
         let favoritesNavigationController = UINavigationControllerFactory.make(with: favoritesListViewController)
-        
+
         let viewControllers = [moviesNavigationController, favoritesNavigationController]
-        
+
         let tabBarController = UITabBarControllerFactory.make(with: viewControllers)
         
         let screen = UIScreen.main
-        
+
         let window = UIWindow(frame: screen.bounds)
         window.rootViewController = tabBarController
         window.backgroundColor = UIColor.white
         window.makeKeyAndVisible()
-        
+
         self.window = window
         return true
     }
