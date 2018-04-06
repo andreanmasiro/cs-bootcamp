d900008c3e929d566eea9afc6dfc56d50a6af4e4
Andre Rodrigues <andre.rodrigues@concrete.com.br>
2018-04-06 17:24:04 -0300

    Add favorite movies list core data gateway tests

---
+++ CsBootcampTests/Gateways/FavoriteMoviesList/FavoriteMoviesListCoreDataGatewaySpec.swift
@@ -0,0 +1,128 @@
+//
+//  FavoriteMoviesListCoreDataGatewaySpec.swift
+//  CsBootcampTests
+//
+//  Created by Andre Rodrigues on 06/04/18.
+//  Copyright © 2018 Bootcampers. All rights reserved.
+//
+
+import CoreData
+import Quick
+import Nimble
+@testable import CsBootcamp
+
+final class FavoriteMoviesListCoreDataGatewaySpec: QuickSpec {
+    
+    override func spec() {
+        
+        describe("FavoriteMoviesListCoreDataGatewaySpec") {
+            
+            context("when it is initialized") {
+                
+                var favoriteMoviesListCoreDataGateway: FavoriteMoviesListCoreDataGateway!
+                
+                beforeEach {
+                    let coreDataStack = InMemoryCoreDataStack()
+                    favoriteMoviesListCoreDataGateway = FavoriteMoviesListCoreDataGateway(coreDataStack: coreDataStack)
+                }
+                
+                let movie = Movie(id: 0, genreIds: [], title: "", overview: "", releaseDate: Date(), posterPath: "")
+                context("and isMovieFavorite is called") {
+                    
+                    var result: Result<Bool>!
+                    beforeEach {
+                        result = favoriteMoviesListCoreDataGateway.isMovieFavorite(movie)
+                    }
+                    
+                    it("should return false") {
+                        expect(result.value).to(beFalse())
+                    }
+                }
+                
+                context("and fetchMovies is called") {
+                    
+                    var result: Result<[Movie]>!
+                    beforeEach {
+                        result = favoriteMoviesListCoreDataGateway.fetchMovies()
+                    }
+                    
+                    it("should return empty list") {
+                        expect(result.value).to(equal([]))
+                    }
+                }
+                
+                context("and setMovieFavorite true is called") {
+                    
+                    beforeEach {
+                        favoriteMoviesListCoreDataGateway.setMovie(movie, favorite: true)
+                    }
+                    
+                    context("and fetchMovies is called") {
+                        
+                        var result: Result<[Movie]>!
+                        beforeEach {
+                            result = favoriteMoviesListCoreDataGateway.fetchMovies()
+                        }
+                        it("should return added movies") {
+                            expect(result.value).to(equal([movie]))
+                        }
+                    }
+                    
+                    context("and setMovieFavorite false is called") {
+                        
+                        beforeEach {
+                            favoriteMoviesListCoreDataGateway.setMovie(movie, favorite: false)
+                        }
+                        
+                        context("and fetchMovies is called") {
+                            
+                            var result: Result<[Movie]>!
+                            beforeEach {
+                                result = favoriteMoviesListCoreDataGateway.fetchMovies()
+                            }
+                            it("should return empty list") {
+                                expect(result.value).to(equal([]))
+                            }
+                        }
+                    }
+                }
+                
+                context("and toggleMovieFavorite is called") {
+                    
+                    beforeEach {
+                        favoriteMoviesListCoreDataGateway.toggleMovieFavorite(movie)
+                    }
+                    
+                    context("and fetchMovies is called") {
+                        
+                        var result: Result<[Movie]>!
+                        beforeEach {
+                            result = favoriteMoviesListCoreDataGateway.fetchMovies()
+                        }
+                        it("should return added movies") {
+                            expect(result.value).to(equal([movie]))
+                        }
+                    }
+                    
+                    context("and toggleMovieFavorite is called") {
+                        
+                        beforeEach {
+                            favoriteMoviesListCoreDataGateway.toggleMovieFavorite(movie)
+                        }
+                        
+                        context("and fetchMovies is called") {
+                            
+                            var result: Result<[Movie]>!
+                            beforeEach {
+                                result = favoriteMoviesListCoreDataGateway.fetchMovies()
+                            }
+                            it("should return empty list") {
+                                expect(result.value).to(equal([]))
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
